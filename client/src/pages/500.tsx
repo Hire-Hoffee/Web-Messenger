94e4feb2d4612e99b177054f5db5f074af866a76
Rostislav Alekseev <mr.rostik.01@gmail.com>
2023-07-24 00:33:18 +0200

    500 error page

---
+++ client/src/pages/500.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import { ErrorOutline } from "@mui/icons-material";
+import Link from "next/link";
+
+type Props = {};
+
+export default function Error({}: Props) {
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "24px",
+      }}
+    >
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Typography sx={{ fontSize: "72px", color: "error.light" }}>
+          Internal server error (500)
+        </Typography>
+        <ErrorOutline sx={{ fontSize: "150px", color: "error.main", marginLeft: "30px" }} />
+      </Box>
+      <Link href="/" style={{ marginTop: "20px" }}>
+        Return to the home page
+      </Link>
+    </Box>
+  );
+}
