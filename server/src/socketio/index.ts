0fa22b18c6f4f8cf84b5c27da636b5bfaad1c2b5
Rostislav Alekseev <mr.rostik.01@gmail.com>
2025-02-05 13:15:28 +0200

    socketio multiple hosts

--- server/src/socketio/index.ts
+++ server/src/socketio/index.ts
@@ -1,10 +1,10 @@
 import { Server } from "socket.io";
 import type { Server as httpServer } from "http";
 
-export default function (server: httpServer, host: string) {
+export default function (server: httpServer, hosts: string | string[]) {
   return new Server(server, {
     cors: {
-      origin: host,
+      origin: hosts,
     },
   });
 }
